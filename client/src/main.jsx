991d1937e1c5d8012ef4eb61227c2de8287f8d23
dev-aditya-lab <ad1123itya@gmail.com>
2026-03-26 00:07:34 +0530

    feat: Implement initial React application structure with routing and authentication flow.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <p style={{ color: 'red', textAlign: 'center', padding: '10px' }}>Guys! The backend is hosted on rander free plan so that frist time it will take 30-60 sec to load</p>
     <App />
   </StrictMode>,
 )
